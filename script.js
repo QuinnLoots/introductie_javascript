5051483c50e343442916fbfe27072d724f2e87ce
Quinn Loots <loots.quinn@gmail.com>
2018-12-05 16:34:19 +0100

    tot aan objecten oefening 2

--- script.js
+++ script.js
@@ -123,10 +123,98 @@ for (let i=1;i<=5;i++){
     teken = teken + "* ";
     console.log(teken);
 }
+// oefening 1 arrays
+numberArray=[1, 2, 3, 3, 4, 5, 6, 7, 8, 9];
+let total=0;
+for (i=0;i<numberArray.length;i++){
+total = total + numberArray[i];
+}
+console.log(total);
+
+//oefening 2 arrays
+
+acteurArray=["Stalone","Cruiz","Quinn"];
+for (i=0;i<acteurArray.length;i++){
+    console.log("Nummer "+(i+1)+" is " +acteurArray[i]);
+}
+
+// oefening 3 arrays
+
+marioArray=["Mario", "Luigi", "Peach"];
+marioArray2=marioArray;
+marioArray2.push("Bowser");
+
+console.log(marioArray2);
+
+// oefening 1 math
+
+arrayMin=[7, 5, -12, 6, 32, 18, 14, -2];
+min = Math.min(...arrayMin);
+console.log(min);
 
 
+arrayMax=[-3, 9, 21, 36, 27, 54, 17, 35];
+max=Math.max(...arrayMax)
+console.log(max);
+
+console.log(min+max);
+
+//oefening 2 math
+
+floatSpeed=10.4;
+lowSpeed= Math.floor(floatSpeed);
+highSpeed= Math.ceil(floatSpeed);
 
 
+//oefening 3 math
 
+console.log(Math.floor((Math.random() * 51) + 50));
+console.log(Math.floor((Math.random() * 2) + 0));
+console.log(Math.floor((Math.random() * 11) + 0));
 
+//Math.floor(Math.random() * (max - min + 1)) + min;
 
+//oefening 4 math
+
+namenArray= ["Goro", "Johnny Cago", "Kano", "Liu Kano", "Raiden", "Raptile", "Scorpion", "Shang Tsun", "Sonya", "Sub-Zero"];
+willekeurig = Math.floor(Math.random() * (namenArray.length + 1));
+console.log(namenArray[willekeurig]);
+
+//oefening 1 objecten
+
+let character = {
+        name : "Quinn",
+        age : 23,
+        items_to_give: ["paper","remote control","USB stick"],
+        giveItem : function(a){
+            console.log( this.items_to_give[a] );   
+        }
+    };
+
+for(let key in character){                 
+        console.log( key );                
+        console.log( character[key] );         
+    }
+randomNumber = Math.floor(Math.random() * (3));
+character.giveItem(randomNumber);
+
+//oefening 2 objecten
+
+function MyFirstConstructor(titel, fysiek, magie, level, beschikbaar) {
+        this.title = titel; 
+        this.physic = fysiek;
+        this.magic = magie;
+        this.minLevel = level;
+        this.available = beschikbaar;         
+    };
+
+zwaard = MyFirstConstructor("dragonsword",150,20,10,true);
+bijl = MyFirstConstructor("battleaxe",100,0,5,false);
+scepter = MyFirstConstructor("magicscepter",0,150,20,true);
+
+console.log(zwaard.title);
+weaponArray = [zwaard,bijl,scepter];
+
+for (i=0;i<=weaponArray.length;i++){
+    console.log(weaponArray[i].title);
+}
